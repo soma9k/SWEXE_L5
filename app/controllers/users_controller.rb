90013450b579deafd6dff398075a98b4bf90d2c3
soma9k <1911140006x@ed.fuk.kindai.ac.jp>
2021-10-11 08:08:47 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(uid: params[:user][:uid], pass: params[:user][:pass], age: params[:user][:age])
+    if @user.save
+      flash[:notice] = 'userを追加しました'
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+     flash[:notice] = 'userを削除しました'
+    end
+    redirect_to users_path
+  end
+end
