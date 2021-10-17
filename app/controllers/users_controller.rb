278e99de0da31acb0c517b8ee026b940c406f11e
soma9k <1911140006x@ed.fuk.kindai.ac.jp>
2021-10-17 07:46:06 +0000

    プロフィール追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 
   def create
-    @user = User.new(uid: params[:user][:uid], pass: BCrypt::Password.create(params[:user][:pass]))
+    @user = User.new(uid: params[:user][:uid], pass: BCrypt::Password.create(params[:user][:pass]), profile: params[:user][:profile])
     if @user.save
       flash[:notice] = 'userを追加しました'
       redirect_to users_path
@@ -16,6 +20,21 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(profile: params[:user][:profile])
+      flash[:notice] = 'userを更新しました'
+      redirect_to users_path
+    else
+      render 'edit'
+    end
+  end
+  
 
   def destroy
     user = User.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_071823) do
+ActiveRecord::Schema.define(version: 2021_10_16_081121) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_10_11_071823) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile"
   end
 
 end
